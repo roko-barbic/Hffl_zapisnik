1031ef00b02313e691db8d58e64211641b2bc490
rokobarbic <rokobarosk12345@gmail.com>
2026-03-16 02:09:37 +0100

    bug fix for archiving

--- lib/screens/clubs_players_container.dart
+++ lib/screens/clubs_players_container.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hffl_api/hffl_api.dart';
+import 'package:hffl_zapisnik/cubit/auth_cubit.dart';
 import 'package:hffl_zapisnik/cubit/clubs_cubit.dart';
 import 'package:hffl_zapisnik/cubit/season_cubit.dart';
 import 'package:hffl_zapisnik/screens/clubs_players_screen.dart';
@@ -29,22 +30,26 @@ class _ClubsPlayersContainerState extends State<ClubsPlayersContainer> {
   }
 
   void archivePlayer(int playerId){
-    showDialog(
-      context: context,
-      builder: (_) => AlertDialog(
-        title: const Text("Archive Player"),
-        content: const Text("Are you sure you want to archive?\nPlayer will be visible in the when you press plus button on top right corner of this screen"),
-        actions: [
-          TextButton(onPressed: () => Navigator.pop(context), child: const Text("Cancel")),
-          TextButton(
-            onPressed: () {
-              context.read<ClubsCubit>().archivePlayer(playerId);
-            },
-            child: const Text("Archive"),
-          ),
-        ],
-      ),
-    );
+    bool isLoggedIn = context.read<AuthCubit>().state.isLoggedIn;
+    bool isCurrentSeason = context.read<SeasonCubit>().state.selectedSeason == DateTime.now().year;
+    if(isLoggedIn && isCurrentSeason){
+      showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+          title: const Text("Archive Player"),
+          content: const Text("Are you sure you want to archive?\nPlayer will be visible in the when you press plus button on top right corner of this screen"),
+          actions: [
+            TextButton(onPressed: () => Navigator.pop(context), child: const Text("Cancel")),
+            TextButton(
+              onPressed: () {
+                context.read<ClubsCubit>().archivePlayer(playerId);
+              },
+              child: const Text("Archive"),
+            ),
+          ],
+        ),
+      );
+    }
   }
 
   void createNewPlayer(){
